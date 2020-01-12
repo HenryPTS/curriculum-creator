9b058ac2e803fe731352e887d195addea9b465a0
Henry Steinberg <steinbergvhenry@gmail.com>
2020-01-12 00:44:44 +0000

    syntax

--- src/lib/crypto.js
+++ src/lib/crypto.js
@@ -2,12 +2,13 @@ const crypto = require('crypto')
 
 function createHash(data) {
   return new Promise((resolve, reject) => {
-    if (Array.isArray(data))
-      data = data.join('__')
-    const hash = crypto.createHash('md5', 'hex').update(data, 'utf8').digest('hex')
-    if (!hash)
-      return reject('No hash')
-    resolve(hash)
+    if (Array.isArray(data)) data = data.join('__')
+    const hash = crypto
+      .createHash('md5', 'hex')
+      .update(data, 'utf8')
+      .digest('hex')
+    if (!hash) return reject('No hash')
+    return resolve(hash)
   })
 }
 
