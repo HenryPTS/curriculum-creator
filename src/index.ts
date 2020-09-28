f143630bf0d8145718e2fe079d720642f324c476
Henry Steinberg <steinbergvhenry@gmail.com>
2020-09-28 01:35:26 +0100

    loose changes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import HTML5ToPDF from 'html5-to-pdf'
-import { program } from 'commander'
+import commander, { program } from 'commander'
 
 import { getDateString, assetsBase, outBase } from './utils'
 import { readFile, writeFile } from './lib/fs'
@@ -14,21 +14,23 @@ program.parse(process.argv)
 
 async function curriculum() {
   const outputPath = 'curriculum' + getDateString() + '.pdf'
-  var infoKeyMd5 = null
+  var oldInfoKey = null
   
   const styleCss = await readFile(assetsBase('style.css'))
   const viewJson = await readFile(assetsBase('view.json'))
   const indexMst = await readFile(assetsBase('index.mst'))
   if (program.verbose) console.log(`imported: ${assetsBase('style.css')}\n${assetsBase('view.json')}\n${assetsBase('index.mst')}`)
   try {
-    infoKeyMd5 = await readFile(outBase('.info_key.md5'))
+    oldInfoKey = await readFile(outBase('.info_key.md5'))
   } catch ({ error, path }) {
     console.log('No hashtag found in output directory')
   }
 
   const renderedTemplate = await renderBody(indexMst, viewJson)
   const infoKey = await createHash([renderedTemplate, styleCss])
-  if (infoKeyMd5 === infoKey)
+  if (program.verbose)
+    console.log('Created hash key', infoKey)
+  if (oldInfoKey === infoKey && !program.force)
     return console.log('Files unchanged since last compile')
 
   const html5ToPDF = new HTML5ToPDF({
